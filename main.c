493fefcff3c272f5f2256d5eb1b7adca9c9357e8
MOSAICSMarvels <130515800+MOSAICSMarvels@users.noreply.github.com>
2023-04-11 17:56:03 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    enable_servos();
+    set_servo_position(0,1885);
+    set_servo_position(2,1147);
+   
+    wait_for_light(1);
+    shut_down_in(119);
+    
+    motor(0,75);
+    motor(3,75);
+    msleep(1500);
+    
+    ao();
+    
+    msleep(500);
+    
+    motor(0,0);
+    motor(3,75);
+    msleep(1950);
+    
+    motor(0,75);
+    motor(3,75);
+    msleep(5200);
+    
+    motor(0,75);
+    motor(3,0);
+    msleep(2050);
+    
+    motor(0,75);
+    motor(3,75);
+    msleep(2300);
+    ao();
+    
+    set_servo_position(0,1403);
+    msleep(1000);
+    
+    
+    set_servo_position(2,1694);
+    msleep(1000);
+    
+    
+    motor(0,-75);
+    motor(3,-75);
+    msleep(3000);
+    
+    disable_servos();
+    
+   return 0;
+}
